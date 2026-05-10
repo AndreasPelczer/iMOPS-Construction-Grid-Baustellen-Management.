aaf7133cafb5425a5236e544fd84fb8f3ade2f6f
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2026-05-10 12:01:01 +0200

    feat: add OCRService for BuildIQ text recognition

---
+++ iMOPS-Construction-Grid-Baustellen-Management./Service/OCRService.swift
@@ -0,0 +1,20 @@
+import Vision
+import UIKit
+
+// MARK: - OCRService
+class OCRService {
+    func performOCR(on image: UIImage, completion: @escaping ([VNRecognizedTextObservation]) -> Void) {
+        guard let cgImage = image.cgImage else { return }
+
+        let request = VNRecognizeTextRequest { request, _ in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            completion(observations)
+        }
+
+        request.recognitionLevel = .accurate
+        request.usesLanguageCorrection = true
+
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        try? handler.perform([request])
+    }
+}
